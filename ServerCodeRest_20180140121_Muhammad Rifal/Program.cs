bf91696d308daacf5c909bfbfb2cab1c473ee142
MuhammadRifal22 <rifalmuhammad797@gmail.com>
2020-12-20 10:23:19 +0700

    Add project files.

---
+++ ServerCodeRest_20180140121_Muhammad Rifal/Program.cs
@@ -0,0 +1,49 @@
+﻿using ServiceRest_20180140121_Muhammad_Rifal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCodeRest_20180140121_Muhammad_Rifal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            Uri address = new Uri("http://localhost:1907/Mahasiswa");
+            WebHttpBinding bind = new WebHttpBinding();
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY), address);
+                hostObjek.AddServiceEndpoint(typeof(ITI_UMY), bind, "");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior(); //Service Runtime Player
+                smb.HttpGetEnabled = true; //untuk mengaktifkan wsdl (dibuka saat development, tidak untuk dibuka)
+                hostObjek.Description.Behaviors.Add(smb);
+                //mex
+                Binding mexbind = MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObjek.AddServiceEndpoint(typeof(IMetadataExchange), mexbind, "mex");
+
+                WebHttpBehavior whb = new WebHttpBehavior();
+                whb.HelpEnabled = true;
+                hostObjek.Description.Endpoints[0].EndpointBehaviors.Add(whb);
+
+                hostObjek.Open();
+                Console.WriteLine("Server is ready!!!!");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
